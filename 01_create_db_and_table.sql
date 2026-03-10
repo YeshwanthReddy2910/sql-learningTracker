c2c24046805b101c17cf3c67e4a4ac8079440156
YeshwanthReddy2910 <yeshwanthreddy248@gmail.com>
2026-03-10 20:47:33 +0530

    created database and employees table

---
+++ 01_create_db_and_table.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS company_db;
+
+USE company_db;
+
+CREATE TABLE employees2 (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+
+SHOW TABLES;
+
+
+
